3493baed372ae93bf680bc394884f8dc8bed1979
LukaszPiorecki <lukaszpiorecki5@gmail.com>
2022-08-14 13:17:46 +0200

    'release'

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   author = 'Łukasz Piórecki',                   # Type in your name
   author_email = 'lukaszpiorecki5@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/CH4LLENG3R/modelAnalsys.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/CH4LLENG3R/modelAnalsys/archive/refs/tags/tools.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/CH4LLENG3R/modelAnalsys/archive/refs/tags/Alpha.tar.gz',    # I explain this later on
   keywords = ['AI', 'Analsys', 'tool', 'NeuralNetwork', 'DeepLearning'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
